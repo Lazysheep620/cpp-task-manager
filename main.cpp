aff8d6ac187f8a583ea43cdcee4e876adb26c792
Lazysheep620 <2140491487@qq.com>
2026-09-25 22:21:40 +0800

    Refactor task manager with OOP design

--- main.cpp
+++ main.cpp
@@ -3,14 +3,67 @@
 #include<string>
 #include<fstream>
 using namespace std;
-int main(){
+class TaskManager{
+private:
 	vector<string> tasks;
-	ifstream inputFile("tasks.txt");
-	string savedTask;
-	while(getline(inputFile,savedTask)){
-		tasks.push_back(savedTask);
+	void saveTasks(){
+		ofstream outputFile("tasks.txt");
+		
+		for(int i=0;i<tasks.size();i++){
+			outputFile<<tasks[i]<<endl;
+		}
+		
+		outputFile.close();
 	}
-	inputFile.close();
+	void loadTasks(){
+		ifstream inputFile("tasks.txt");
+		string savedTask;
+		
+		while(getline(inputFile,savedTask)){
+			tasks.push_back(savedTask);
+		}
+		
+		inputFile.close();
+	}
+public:
+	TaskManager(){
+		loadTasks();
+	}
+	void addTask(){
+		string task;
+		cin.ignore();
+		cout<<"请输入一个任务：";
+		getline(cin,task);
+		tasks.push_back(task);
+		saveTasks();
+	}
+	void showTasks(){
+		if(tasks.empty()){cout<<"暂无任务"<<endl;}
+		else{
+			cout<<"当前共有"<<tasks.size()<<"个任务："<<endl;
+			for(int i=0;i<tasks.size();i++){
+				cout<<i+1<<". "<<tasks[i]<<endl;
+			}
+		}
+	}
+	void deleteTask(){
+		int number;
+		cout << "请输入要删除的任务编号：";
+		cin>>number;
+		if (number >= 1 && number <= tasks.size()) {
+			tasks.erase(tasks.begin() + number - 1);
+			saveTasks();
+			cout << "删除成功！" << endl;
+			cout<<"当前剩余"<<tasks.size()<<"个任务"<<endl;
+		}
+		else {
+			cout << "任务编号不存在！" << endl;
+		}
+		
+	}
+};
+int main(){
+	TaskManager manager;
 	int choice;
 	while(true){
 		cout<<"===== Task Manager =====" <<endl;
@@ -21,39 +74,13 @@ int main(){
 		cin>>choice;
 		if(choice==4){break;}
 		else if(choice==1){
-			string task;
-			cout<<"请输入一个任务：";
-			cin.ignore();
-			getline(cin,task);
-			tasks.push_back(task);
-			
-			ofstream outputFile("tasks.txt",ios::app);
-			outputFile<<task<<endl;
-			outputFile.close();
-			
-			cout<<"当前共有"<<tasks.size()<<"个任务"<<endl;
+			manager.addTask();
 		}
 		else if(choice==2){
-			for(int i=0;i<tasks.size();i++){
-				cout<<i+1<<". "<<tasks[i]<<endl;
-			}
+			manager.showTasks();
 		}
 		else if(choice==3){
-			int number;
-			cout << "请输入要删除的任务编号：";
-			cin>>number;
-			if (number >= 1 && number <= tasks.size()) {
-				tasks.erase(tasks.begin() + number - 1);
-				ofstream outputFile("tasks.txt");
-				for(int i=0;i<tasks.size();i++){
-					outputFile<<tasks[i]<<endl;
-				}
-				outputFile.close();
-				cout << "删除成功！" << endl;
-			}
-			else {
-				cout << "任务编号不存在！" << endl;
-			}
+			manager.deleteTask();
 		}
 	}
 	return 0;
